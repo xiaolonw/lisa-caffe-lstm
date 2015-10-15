2862a00eb9a6cf39c23ed51e7d4eef317538b11c
xiaolonw <xiaolonw@cs.cmu.edu>
2015-10-14 20:56:17 -0400

    scripts

--- video_script/flow_zero_cls/run_lstm_flow.sh
+++ video_script/flow_zero_cls/run_lstm_flow.sh
@@ -2,8 +2,7 @@
 
 TOOLS=../../build/tools
 
-export HDF5_DISABLE_VERSION_CHECK=1
-export PYTHONPATH=.
-#for debugging python layer
-GLOG_logtostderr=1  $TOOLS/caffe train -solver lstm_solver_flow.prototxt -weights single_frame_all_layers_hyb_flow_iter_50000.caffemodel  
+
+GLOG_logtostderr=1  $TOOLS/caffe train -solver lstm_solver_flow.prototxt -weights /nfs.yoda/xiaolonw/models/ai2_vgg16_flow_zero/video__iter_30000.caffemodel 
+
 echo "Done."
