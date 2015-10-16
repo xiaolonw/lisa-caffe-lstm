dfc810de4ea3ba483d79ba676b606b705b3d7f5c
xiaolonw <xiaolonw@cs.cmu.edu>
2015-10-15 21:14:06 -0400

    test

---
+++ video_script/flow_zero_cls/run_test_net.sh
@@ -0,0 +1,5 @@
+!/usr/bin/env sh
+
+../../build/tools/test_video_net_prop.bin test_lstm_flow.prototxt    /nfs.yoda/xiaolonw/models/snapshots_lstm_flow_iter_10000.caffemodel  /share/vol2/home/xiaolonw/action_scripts/splits_flow/split1/testimglist_block_imgs_split1.txt  /home/xiaolonw/video_models/results_trans3  1
+
+
